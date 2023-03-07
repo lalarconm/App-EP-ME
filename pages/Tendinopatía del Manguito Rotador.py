a4bf7ef98ac4059e9faa6d34008487b4552fa254
Luis Alfredo Alarcon Montero <lalarcon@mutual.cl>
2023-03-07 11:40:48 -0300

    initial commit

---
+++ pages/Tendinopatía del Manguito Rotador.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import time
+
+# Titulos y encabezados
+# st.title('Calificador Automático de EP-ME')
+st.markdown("# En proceso de construcción ❄️")
+st.write("Mutual de Seguridad CChC")
+
+
